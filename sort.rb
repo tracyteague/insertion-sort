7426af100045ae2313f2366a0f5c7d0b1fe17978
Tracy Teague <tracy.teague05@gmail.com>
2017-08-09 15:12:45 -0400

    Change for loop to while loop and add small refactors.

--- sort.rb
+++ sort.rb
@@ -1,19 +1,36 @@
-# INITIAL SOLUTION
+# IMPROVED/REFACTORED SOLUTION
 
 def sort(arr)
-  for i in 1..arr.length-1
-    ele_value_at_index = arr[i]
-    ele_index = i
+  i = 1
+  while i < arr.length
+    ele_value_at_index, ele_index = arr[i], i
     while ele_index > 0 && arr[ele_index-1] > ele_value_at_index
-      arr[ele_index] = arr[ele_index-1]
-      ele_index = ele_index - 1
+      arr[ele_index], ele_index = arr[ele_index-1], ele_index - 1
     end
     arr[ele_index] = ele_value_at_index
+    i+=1
   end
   arr
 end
 
 sort([1,2,3,4,5])
+
+# INITIAL SOLUTION
+
+# def sort(arr)
+#   for i in 1..arr.length-1
+#     ele_value_at_index = arr[i]
+#     ele_index = i
+#     while ele_index > 0 && arr[ele_index-1] > ele_value_at_index
+#       arr[ele_index] = arr[ele_index-1]
+#       ele_index = ele_index - 1
+#     end
+#     arr[ele_index] = ele_value_at_index
+#   end
+#   arr
+# end
+
+# sort([1,2,3,4,5])
 
 
 
